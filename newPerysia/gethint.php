c4f1d765b8f8a83c12883dd5ec3079b35b03604d
perysia <38452981+perysia@users.noreply.github.com>
2018-04-24 14:59:06 +0700

    Add files via upload

---
+++ newPerysia/gethint.php
@@ -0,0 +1,30 @@
+<?php
+$a[] = "Petrus Rynanson Sianipar";
+$a[] = "Kurnia Prijiandi";
+$a[] = "As Daniel";
+$a[] = "Buk Dwi Manis";
+$a[] = "Cistin";
+$a[] = "Melisa";
+$a[] = "Kurnia Prijiandi";
+
+//get the q parameter from URl
+$q = $_REQUEST["q"];
+$hint = "";
+
+//lookup all hints from array if $q is different form ""
+if ($q !== ""){
+	$q = strtolower($q);
+	$len = strlen($q);
+	foreach ($a as $name) {
+		if (stristr($q, substr($name, 0, $len))){
+			if ($hint === ""){
+				$hint = $name;
+			} else {
+				$hint .= ", $name";
+			}
+		}
+	}
+}
+//output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "no suggestion" : $hint;
+?>
